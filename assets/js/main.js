1261f57feab0ffe2b57526724b600682f789861c
Salvatore-Gianfrancesco <salvatore.gianfrancesco@yahoo.it>
2022-10-12 15:16:01 +0200

    added click event - [milestone 3]

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,25 @@ for (let i = 1; i < slideList.length; i++) {
     const slideMarkup = `<img src="${slideList[i]}" alt="">`
     slidesEl.insertAdjacentHTML("beforeend", slideMarkup);
 }
+
+let activeSlide = 0;
+
+const nextSlide = document.querySelector("button:first-of-type")
+    .addEventListener("click", function () {
+        console.log("prev");
+        let currentSlide = document.querySelector(".active");
+        currentSlide.classList.remove("active");
+        activeSlide--;
+        currentSlide = document.querySelector(`img:nth-child(${activeSlide})`);
+        currentSlide.classList.add("active");
+    })
+
+const prevSlide = document.querySelector("button:last-of-type")
+    .addEventListener("click", function () {
+        console.log("next");
+        let currentSlide = document.querySelector(".active");
+        currentSlide.classList.remove("active");
+        activeSlide++;
+        currentSlide = document.querySelector(`img:nth-child(${activeSlide})`);
+        currentSlide.classList.add("active");
+    })
